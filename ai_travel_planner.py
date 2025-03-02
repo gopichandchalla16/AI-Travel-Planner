c8a88329c42a7e97bbf742c4c94571f0672b970b
Gopichand <162360009+gopichandchalla16@users.noreply.github.com>
2025-03-02 13:01:33 +0530

    ai_travel_planner.py

--- ai_travel_planner.py
+++ ai_travel_planner.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 import os
 
 # Set API key from Streamlit Secrets
-API_KEY = st.secrets["GENAI_API_KEY"]
+API_KEY = st.secrets["AIzaSyB1E2cCnRDAbX_siKss8NFaeKRKlK2e6ko"]
 
 # Custom Theme
 st.set_page_config(
